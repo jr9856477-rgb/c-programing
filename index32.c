5e60f97084e410449a6651999bd2f0bd442270a4
jr9856477-rgb <jr9856477@gmail.com>
2025-12-03 10:26:01 +0530

    Add files via upload

---
+++ index32.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+struct student {
+ int id;
+ char name[20];
+};
+void display(struct stud s[],int n);
+{
+ for(int i=0:i<n;i++)
+    printf("ID:%d,name:%d\n",s[i].name);
+}
+int main() {
+struct stud s[3]={{101,"shreekanth"},{102,tharun"},{103,"shree"}};
+display(s,3);
+return o;
+}
+
